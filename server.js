8098500e5659615a6b2e0685421c718f22abbd6a
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-06 11:11:34 -0400

    Added connect to database

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const inquirer = require("inquirer");
+//Import and require mysql2
 const mysql = require("mysql2");
 
+// Connect to database
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '',
+      database: 'employeeTracker_db'
+    },
+    console.log(`Connected to the employeeTracker_db database.`)
+  );
+  
 // Function to Start Employee Tracker Application
 function start() {
   inquirer
