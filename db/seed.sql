98e97bb31d088ff70d5615774abb84934d702145
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-12 21:41:36 -0400

    Change code in seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,36 +1,32 @@
 -- Inserting records into the 'departments' table
-INSERT INTO departments (department_name)
+INSERT INTO department (department_name)
 VALUES 
-('Executive Board'),
 ('Marketing & Sales'),
-('Human Resources'),
 ('Finance & Accounting'),
-('Engineering'),
 ('Information Technology'),
-('Legal'),
-('Maintenance');
+('Operations');
 
 -- Inserting records into the 'roles' table
 INSERT INTO role (title, salary, department_id)
 VALUES 
-('Chief Executive Officer', 555000.00, 1),
-('Marketing Manager', 125000.00, 2),
-('HR Director', 189000.00, 3),
-('Finance Head', 145000.00, 4),
-('Senior Engineer', 185000.00, 5),
-('IT Manager', 125000.00, 6),
-('Legal Manager', 95000.00, 7),
-('Maintenance Manager', 135000.00, 8);
+('Sales Lead', 90000.00, 1),
+('Marketing Manager', 125000.00, 1),
+('Finance Head', 15000.00, 2),
+('Accountant',10000,2)
+('Senior Engineer', 185000.00, 3),
+('IT Manager', 125000.00, 3),
+('Project Manager', 95000.00, 4),
+('Operation Manager', 35000.00, 4);
 
 -- Inserting records into the 'employee' table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('James', 'Antezana', 1, 1),
-('Kim', 'Hurt', 2, 2),
+('Kim', 'Hurt', 2, null),
 ('David', 'Calle', 3, 3),
-('Ashley', 'Moore', 4, 4),
+('Ashley', 'Moore', 4, null),
 ('Carlos', 'Hobbes', 5, 5),
-('Mary', 'Neumann', 6, 6),
+('Mary', 'Neumann', 6, null),
 ('Tom', 'Holland', 7, 7),
-('Ana', 'Ford', 8, 8);
+('Ana', 'Ford', 8, null);
 
